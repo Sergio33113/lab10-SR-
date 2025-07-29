736c418ee8721f78e70de6d74de9d7a8fd732924
Sergio33113 <145488179+Sergio33113@users.noreply.github.com>
2025-07-29 11:20:36 -0400

    Update test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -1,51 +1,56 @@
 # https://github.com/Sergio33113/lab10-SR-
 # Partner 1: Sergio Rivera
 
+
 import unittest
 import calculator
 import math
 
 class TestCalculator(unittest.TestCase):
-
+    
     def test_add(self):
         self.assertEqual(calculator.add(2, 3), 5)
 
+   
     def test_subtract(self):
-        self.assertEqual(calculator.subtract(5, 3), 2)
+        self.assertEqual(calculator.sub(10, 4), 6)
 
-    def test_mul(self):
-        self.assertEqual(calculator.mul(4, 3), 12)
-
-    def test_div(self):
-        self.assertEqual(calculator.div(10, 2), 5)
-        with self.assertRaises(ValueError):
+   
+    def test_divide_by_zero(self):
+        with self.assertRaises(ZeroDivisionError):
             calculator.div(5, 0)
 
-    def test_mod(self):
-        self.assertEqual(calculator.mod(10, 3), 1)
-        with self.assertRaises(ValueError):
-            calculator.mod(10, 0)
-
-    def test_power(self):
-        self.assertEqual(calculator.power(2, 3), 8)
-
+    
     def test_logarithm(self):
-        self.assertAlmostEqual(calculator.logarithm(math.e), 1, places=5)
-        self.assertAlmostEqual(calculator.logarithm(100, 10), 2)
+        self.assertAlmostEqual(calculator.log(100, 10), 2)
+
+   
+    def test_log_invalid_base(self):
         with self.assertRaises(ValueError):
-            calculator.logarithm(-1)
+            calculator.log(100, 1)
 
-    def test_exp(self):
-        self.assertAlmostEqual(calculator.exp(1), math.e, places=5)
+   
+    def test_multiply(self):
+        self.assertEqual(calculator.mul(3, 4), 12)
 
-    def test_square_root(self):
+   
+    def test_divide(self):
+        self.assertEqual(calculator.div(10, 2), 5)
+
+   
+    def test_log_invalid_argument(self):
+        with self.assertRaises(ValueError):
+            calculator.log(-1, 10)
+
+    
+    def test_hypotenuse(self):
+        self.assertAlmostEqual(calculator.hypotenuse(3, 4), 5)
+
+   
+    def test_sqrt(self):
         self.assertEqual(calculator.square_root(25), 5)
         with self.assertRaises(ValueError):
             calculator.square_root(-4)
 
-    def test_hypotenuse(self):
-        self.assertEqual(calculator.hypotenuse(3, 4), 5)
-
 if __name__ == '__main__':
     unittest.main()
-
