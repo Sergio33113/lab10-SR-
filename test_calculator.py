8079cf33a58b872bf8e553bf8bdec0f83230d43c
Sergio33113 <145488179+Sergio33113@users.noreply.github.com>
2025-07-29 11:35:04 -0400

    Update test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -1,6 +1,6 @@
 # https://github.com/Sergio33113/lab10-SR-
 # Partner 1: Sergio Rivera
-# Partner 2: Sergio Rivera
+
 
 import unittest
 import calculator
@@ -11,42 +11,33 @@ class TestCalculator(unittest.TestCase):
     def test_add(self):
         self.assertEqual(calculator.add(2, 3), 5)
 
-   
     def test_subtract(self):
         self.assertEqual(calculator.sub(10, 4), 6)
 
-   
     def test_divide_by_zero(self):
         with self.assertRaises(ZeroDivisionError):
             calculator.div(5, 0)
 
-    
     def test_logarithm(self):
-        self.assertAlmostEqual(calculator.log(100, 10), 2)
+        self.assertAlmostEqual(calculator.logarithm(100, 10), 2)
 
-   
     def test_log_invalid_base(self):
         with self.assertRaises(ValueError):
-            calculator.log(100, 1)
+            calculator.logarithm(100, 1)
 
-   
     def test_multiply(self):
         self.assertEqual(calculator.mul(3, 4), 12)
 
-   
     def test_divide(self):
         self.assertEqual(calculator.div(10, 2), 5)
 
-   
     def test_log_invalid_argument(self):
         with self.assertRaises(ValueError):
-            calculator.log(-1, 10)
+            calculator.logarithm(-1, 10)
 
-    
     def test_hypotenuse(self):
         self.assertAlmostEqual(calculator.hypotenuse(3, 4), 5)
 
-   
     def test_sqrt(self):
         self.assertEqual(calculator.square_root(25), 5)
         with self.assertRaises(ValueError):
