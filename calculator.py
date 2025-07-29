dce9e1152dcfc77804bec09919028f80970db07c
Sergio33113 <145488179+Sergio33113@users.noreply.github.com>
2025-07-29 11:05:46 -0400

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,11 +1,25 @@
-"""
-calculator.py
-- Defines functions used to create a simple calculator
+# https://github.com/Sergio33113/lab10-SR-
+# Partner 1: Sergio Rivera
+# Partner 2: 
 
-One function per operation, in order.
-"""
-# First example
-def add(a, b): 
-    pass
+def add(x, y):
+    return x + y
 
+def subtract(x, y):
+    return x - y
 
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y == 0:
+        raise ValueError("Cannot divide by zero.")
+    return x / y
+
+def power(x, y):
+    return x ** y
+
+def mod(x, y):
+    if y == 0:
+        raise ValueError("Cannot mod by zero.")
+    return x % y
